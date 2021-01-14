121411df737e87f5dd3846b34aab122b5e25d891
jules.etourneau <jules.etourneau@stchales-stecroix.org>
2021-01-14 11:12:07 +0100

    premier commit

---
+++ database/migrations/2021_01_13_105737_create_properties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo');
+            $table->integer('price');
+            $table->string('location');
+            $table->integer('surface');
+            $table->integer('roomNumber');
+            $table->string('condition');
+            $table->integer('constructionYear');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
